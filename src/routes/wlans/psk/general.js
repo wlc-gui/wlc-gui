9fd0abaf56a02c8ee5ba63cc88b94e3443afbf27
pce0303 <ivychoen@gmail.com>
2024-02-04 22:57:02 +0900

    wlans/psk/general

---
+++ src/routes/wlans/psk/general.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function General() {
+  return (
+    <>
+    <TitleContainer>
+      <Title>Profile Name</Title>
+      <Title>Type</Title>
+      <Title>SSID</Title>
+      <Title>Status</Title>
+      <Title>Security Policies</Title>
+    </TitleContainer>
+    <DetailContainer>
+      <Detail>Internal</Detail>
+      <Detail>Wireless LAN</Detail>
+      <Detail>Internal</Detail>
+      <Detail>
+        <Checkbox type="checkbox"/>
+        <CheckboxLabel>Enabled</CheckboxLabel>
+      </Detail>
+      <Detail>{}</Detail>
+    </DetailContainer>
+    </>
+  );
+}
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: baseline;
+  flex-direction: column;
+  gap: 50px;
+  width: 50%;
+`;
+
+const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  width: 50%;
+`;
+
+const Title = styled.div`
+  color: #000;
+  font-size: 120%;
+`;
+
+const Detail = styled.div`
+  color: #a8a4a5;
+  font-size: 120%;
+  display: flex;
+  align-items: center;
+`;
+
+const Checkbox = styled.input`
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border: solid 2px rgb(211, 211, 211);
+  border-radius: 50%;
+  outline: none;
+  cursor: pointer;
+  background-color: #fff;
+  margin-right: 10px;
+  flex-shrink: 0;
+  transition: all ease 0.4s;
+
+  &:checked {
+    background-color: #45de45;
+  }
+`;
+
+const CheckboxLabel = styled.span`
+  font-size: 100%;
+  color: #a8a4a5;
+`;
